bcacccf7858e8807c8e3b5c9c56572d7828eae7e
Ezequiel-GM <ezequielgoldsmithmorgan@gmail.com>
2021-09-16 17:01:53 -0700

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 import AuthRouter from "./AuthRouter";
 
 const AppBackground = styled.div`
   height: 100%;
   background-color: ${({ theme }) => theme.color.appBackground};
-`
+`;
 
 export default function App() {
   return (
     <AppBackground>
       <AuthRouter />
     </AppBackground>
-  )
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from "styled-components";
 
 import App from "./App";
-import theme from './theme.json';
+import theme from "./theme.json";
 
 import "./index.css";
 
