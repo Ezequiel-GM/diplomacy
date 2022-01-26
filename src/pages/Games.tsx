fe75cb3a423716cda357f046b3a0792109aa6936
Ezequiel-GM <ezequielgoldsmithmorgan@gmail.com>
2022-01-25 16:57:20 -0800

    games page background

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,3 +1,30 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+const PageMotion = styled(motion.main)`
+  min-height: 100%;
+  background-color: ${({ theme }) => theme.color.card};
+`;
+
+const visibilityVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delayChildren: 0.5,
+    },
+  },
+};
+
 export default function Games() {
-  return null;
+  return (
+    <PageMotion
+      variants={visibilityVariants}
+      initial="hidden"
+      animate="visible"
+    ></PageMotion>
+  );
 }
